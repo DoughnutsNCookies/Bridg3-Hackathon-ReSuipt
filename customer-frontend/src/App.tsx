726b7186db96a3889f054cc236280c2b2d31fd56
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-07-27 12:53:58 +0800

    init basic pages

--- customer-frontend/src/App.tsx
+++ customer-frontend/src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="text-5xl">Hello world</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Google from "./pages/google";
+import Login from "./pages/login";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/google" element={<Google />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
---
+++ customer-frontend/src/pages/google.tsx
@@ -0,0 +1,5 @@
+function Google() {
+  return <h1>Google</h1>;
+}
+
+export default Google;
