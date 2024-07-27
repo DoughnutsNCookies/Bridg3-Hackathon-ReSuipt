042e8fa8bcb6dc45206a9d8d4a78c0c8901fb640
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-07-27 16:16:04 +0800

    Added logo

--- merchant-frontend/vite.config.ts
+++ merchant-frontend/vite.config.ts
@@ -16,13 +16,8 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "512.png",
-            sizes: "512x512",
+            src: "logo.png",
+            sizes: "500x500",
             type: "image/png",
           },
         ],
