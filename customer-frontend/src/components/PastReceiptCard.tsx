840f34ef4dc91fdd028977e38bb5753fd89c3e13
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-07-28 00:46:07 +0800

    Update to use nextui card

--- customer-frontend/src/components/PastReceiptCard.tsx
+++ customer-frontend/src/components/PastReceiptCard.tsx
@@ -1,5 +1,13 @@
 import { LuCopy } from "react-icons/lu";
 import { PastReceipt } from "../hooks/usePastReceipts";
+import {
+  Card,
+  CardHeader,
+  Divider,
+  CardBody,
+  CardFooter,
+  Link,
+} from "@nextui-org/react";
 
 interface PastReceiptProps {
   pastReceipt: PastReceipt;
@@ -10,57 +18,57 @@ interface PastReceiptProps {
 const PastReceiptCard = (props: PastReceiptProps) => {
   const { pastReceipt, index, length } = props;
 
-  let total = 0;
-  pastReceipt.data.forEach((v) => (total += v.price));
-
-  const copyTxId = (tx: string) => {
-    navigator.clipboard.writeText(tx);
-  };
-
   return (
     <div className="pt-2 px-2">
-      <div className="border-1 bg-cloud rounded-xl">
-        <div className="text-center pt-4 pb-2 relaive">
-          <div className="absolute left-6">
-            <span className="opacity-30 text-xs">#{length - index}</span>
-          </div>
-          <span className="text-3xl font-semibold">SPH Store</span>
-        </div>
-        <ul className="pt-4 px-4 flex flex-col gap-2">
-          {pastReceipt.data.map((v, i) => (
-            <li className="flex flex-row justify-between" key={i}>
-              <span>{v.name}</span>
-              <div className="flex flex-row gap-2">
-                <span>$ {v.price}</span>
-              </div>
-            </li>
-          ))}
-          <div className="border border-ocean" />
-          <div className="flex flex-row justify-between font-bold">
-            <span>Total</span>
-            <div className="flex flex-row gap-2">
-              <span>$ {total}</span>
+      <Card>
+        <CardHeader className="text-center flex flex-col w-full">
+          <p className="text-3xl font-semibold">SPH Store</p>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <ul className="p-4 flex flex-col gap-2">
+            <div className="flex flex-row justify-between font-bold">
+              <span>Item Name</span>
+              <span>Price</span>
             </div>
-          </div>
-        </ul>
-        <div className="px-4 pb-4 flex justify-between">
+            <div className="border border-ocean" />
+            {pastReceipt.data.map((item) => (
+              <li className="flex flex-row justify-between">
+                <span>{item.name}</span>
+                <span>{item.price}</span>
+              </li>
+            ))}
+            {pastReceipt.data.length === 0 && (
+              <li className="flex flex-row h-[30px] text-ocean/70 items-center justify-center">
+                <span>No items...</span>
+              </li>
+            )}
+            <div className="border border-ocean" />
+            <div className="flex flex-row justify-between font-bold text-lg">
+              <span>Total</span>
+              <span>
+                $ {pastReceipt.data.reduce((acc, item) => acc + item.price, 0)}
+              </span>
+            </div>
+          </ul>
+        </CardBody>
+        <Divider />
+        <CardFooter className="px-4 pb-4 flex justify-between">
           <div className="text-start flex-1 flex items-center gap-1">
-            <span className="text-xs opacity-30">
+            <Link
+              color="primary"
+              size="sm"
+              isExternal
+              href={`https://suiscan.xyz/testnet/object/${pastReceipt.txDigest}`}
+              showAnchorIcon
+            >
               Tx: {pastReceipt.txDigest.slice(0, 6)}....
               {pastReceipt.txDigest.slice(-4)}
-            </span>
-            <span
-              className="text-xs opacity-30"
-              onClick={() => copyTxId(pastReceipt.txDigest)}
-            >
-              <LuCopy />
-            </span>
+            </Link>
           </div>
-          <div className="text-end">
-            <span className="text-xs opacity-30">{pastReceipt.timestamp}</span>
-          </div>
-        </div>
-      </div>
+          <span className="text-sm">{pastReceipt.timestamp}</span>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
