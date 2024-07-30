b7bbc682cf5e8df70cf4272ed428543efb57df89
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-07-30 16:10:43 +0800

    init tailwind

---
+++ home/tailwind.config.js
@@ -0,0 +1,25 @@
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      ocean: "#011829",
+      deepOcean: "#030F1C",
+      aqua: "#C0E6FF",
+      cloud: "#FFFFFF",
+      sea: "#4DA2FF",
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
