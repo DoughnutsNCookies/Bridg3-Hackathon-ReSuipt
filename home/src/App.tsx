f9fe7ab544dfc23573b7d6debfb2190fd7bfd4c7
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2024-07-30 16:48:07 +0800

    Added simple home page

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Image, Link } from "@nextui-org/react";
+import { LuExternalLink } from "react-icons/lu";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="h-screen w-screen bg-aqua flex flex-col text-center items-center justify-center gap-20">
+      <div className="flex flex-col gap-4 items-center justify-center gap-4">
+        <Image src="/logoText.png" width={300} />
+        <p className="text-xl">
+          <strong>Digitalizing receipts </strong>
+          with <br />
+          <strong>seamless user experience</strong>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="flex flex-col gap-8">
+        <p className="text-3xl font-bold">I am a...</p>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col items-center">
+            <Button
+              className="font-bold text-xl"
+              size="lg"
+              fullWidth
+              color="primary"
+              onClick={() => {
+                window.location.href = "https://merchant.resuipt.xyz";
+              }}
+              endContent={<LuExternalLink />}
+            >
+              Merchant
+            </Button>
+            <Link
+              href="https://merchant.resuipt.xyz"
+              underline="always"
+              className="text-ocean"
+            >
+              https://merchant.resuipt.xyz
+            </Link>
+          </div>
+          <div className="flex flex-col items-center">
+            <Button
+              className="font-bold text-xl"
+              size="lg"
+              fullWidth
+              color="primary"
+              onClick={() => {
+                window.location.href = "https://customer.resuipt.xyz";
+              }}
+              endContent={<LuExternalLink />}
+            >
+              Customer
+            </Button>
+            <Link
+              href="https://customer.resuipt.xyz"
+              underline="always"
+              className="text-ocean"
+            >
+              https://customer.resuipt.xyz
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
